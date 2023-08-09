edaf678dab5d31a4627e5ad451cc776277539594
Gergely Aliz <alice.gergely@gmail.com>
2023-08-09 23:00:42 +0300

    idk what i changed but now it works

--- database/connectDatabase.js
+++ database/connectDatabase.js
@@ -3,10 +3,12 @@ import mysql from 'mysql2/promise.js';
 async function createPool() {
   const db = await mysql.createPool({
     host: '34.155.250.34',
+    port: 3306,
     user: 'root',
     password: "j:&9ZJYf'23-6Mh?",
     database: 'library',
     connectionLimit: 100,
+    connectTimeout: 300000
   });
   return db;
 }
