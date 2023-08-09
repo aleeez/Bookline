f251190eb5206c308a37cf973a8db88814cbb519
Gergely Aliz <alice.gergely@gmail.com>
2023-08-09 21:41:46 +0300

    updating database credentials

--- database/connectDatabase.js
+++ database/connectDatabase.js
@@ -2,9 +2,9 @@ import mysql from 'mysql2/promise.js';
 
 async function createPool() {
   const db = await mysql.createPool({
-    host: '34.155.222.250',
+    host: '34.155.250.34',
     user: 'root',
-    password: 'hcY(osHzuh)khdSn',
+    password: "j:&9ZJYf'23-6Mh?",
     database: 'library',
     connectionLimit: 100,
   });
